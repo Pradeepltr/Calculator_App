e424dab57f668632b929065c54482a5994fd4585
Pradeep Kumar <pk6361439@gmail.com>
2021-10-13 08:48:17 +0530

    first commmit

---
+++ index.js
@@ -0,0 +1,29 @@
+var button=document.getElementById("btn1");
+var button=document.getElementById("btn2");
+var button=document.getElementById("btn3");
+var button=document.getElementById("btn4");
+var button=document.getElementById("btn5");
+var button=document.getElementById("btn6");
+var button=document.getElementById("btn7");
+var button=document.getElementById("btn8");
+var button=document.getElementById("btn9");
+var button=document.getElementById("btn0");
+var button=document.getElementById("btn+");
+var button=document.getElementById("btn-");
+var button=document.getElementById("btn*");
+var button=document.getElementById("btn/");
+var button=document.getElementById("btnc");
+var button=document.getElementById("btnce");
+var button=document.getElementById("btn00");
+var button=document.getElementById("btn.");
+var button=document.getElementById("btn+-");
+var p=document.getElementsByTagName('p');
+var result=0;
+var operator;
+var equal=document.getElementById('click');
+button.addEventListener('click',function(){
+  console.log("hello");
+    var a=button.innerText;
+  p.innerText=a;
+    
+});
